a4fc72b2a65cc65102256907cb3f2f50096badcd
Valentine Riedman <underblueskysismile@gmail.com>
2022-06-15 22:34:54 -0500

    dispatch working in feeling

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,24 +1,31 @@
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import{useDispatch} from 'react-redux';
 
 function Feeling( props ){
-
+    const dispatch = useDispatch();
     const history = useHistory();
-    const [ hook, setHook ] = useState( null )
+    const [ feeling, setFeeling ] = useState( '' )
 
-    const next = ()=>{
+    const changeFeeling = (event)=>{
+        console.log(feeling);
+        setFeeling(event.target.value);
+    }
+
+    const addFeeling = ()=>{
+        dispatch({ type:'SEND_FEELING', payload: feeling})
         history.push( '/understanding');
       }
     return (
         <div>
             <h2>How are you feeling today?(1-5)</h2>
-            <select>
+            <select onChange={changeFeeling}>
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
                 <option>4</option>
                 <option>5</option>
-                </select> <button onClick={next}>NEXT</button>
+                </select> <button onClick={addFeeling}>NEXT</button>
         </div>
     );
 }
